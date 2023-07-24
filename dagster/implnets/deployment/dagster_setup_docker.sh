ed812023459f5153fa5d3641d7f083779f112416
David Valentine <david.valentine@gmail.com>
2023-07-24 10:52:23 -0700

    Change the logic of setting up the network

--- dagster/implnets/deployment/dagster_setup_docker.sh
+++ dagster/implnets/deployment/dagster_setup_docker.sh
@@ -28,7 +28,7 @@ fi
 if [ -f $envfile ]
   then
     echo "using " $envfile
-    export $(sed  '/^[ \t]*#/d' $envfile |  sed '/^$/d' | xargs)
+    export $(cat ${envfile} | xargs)
   else
     echo "missing environment file. pass flag, or copy and edit file"
     echo "cp envFile.env .env"
@@ -41,22 +41,22 @@ fi
 ## need to docker (network|volume) ls | grep (traefik_proxy|traefik_proxy) before these calll
 ## or an error will be thrown
 #echo "This message is OK **Error response from daemon: network with name traefik_proxy already exists.** "
-if [  "$(docker network ls  | grep -${GLEANERIO_DOCKER_HEADLESS_NETWORK})" ] ; then
-   echo ${GLEANERIO_DOCKER_HEADLESS_NETWORK} netowrk exists;
+if [  "$(docker network ls  | grep -${GLEANER_HEADLESS_NETWORK})" ] ; then
+   echo ${GLEANER_HEADLESS_NETWORK} netowrk exists;
 else
    echo creating network
    if [ "$(docker info | grep Swarm | sed 's/Swarm: //g')" == "inactive" ]; then
         echo Not Swarm
-        if `docker network create -d bridge --attachable ${GLEANERIO_DOCKER_HEADLESS_NETWORK}`; then
-           echo 'Created network ${GLEANERIO_DOCKER_HEADLESS_NETWORK}'
+        if `docker network create -d bridge --attachable ${GLEANER_HEADLESS_NETWORK}`; then
+           echo 'Created network ${GLEANER_HEADLESS_NETWORK}'
         else
            echo "ERROR: *** Failed to create local network. "
             exit 1
         fi
    else
         echo Is Swarm
-        if `docker network create -d overlay --attachable ${GLEANERIO_DOCKER_HEADLESS_NETWORK}`; then
-          echo 'Created network ${GLEANERIO_DOCKER_HEADLESS_NETWORK}'
+        if `docker network create -d overlay --attachable ${GLEANER_HEADLESS_NETWORK}`; then
+          echo 'Created network ${GLEANER_HEADLESS_NETWORK}'
         else
             echo "ERROR: *** Failed to create swarm network.  "
             exit 1
@@ -67,46 +67,7 @@ fi
 
 #echo NOTE: Verify that the traefik_proxy network  SCOPE is swarm
 
-echo added network  ${GLEANERIO_DOCKER_HEADLESS_NETWORK}
+docker volume create ${GLEANER_CONFIG_VOLUME:-dagster_gleaner_configs}
 
-docker volume create dagster-postgres
-docker volume create dagster-storage
+echo added network  ${GLEANER_HEADLESS_NETWORK}  and volume ${GLEANER_CONFIG_VOLUME}
 
-if [  "$(docker config ls  | grep -${GLEANERIO_GLEANER_CONFIG_PATH})" ] ; then
-   echo ${GLEANERIO_GLEANER_CONFIG_PATH} config exists;
-else
-   echo creating config
-
-      if `docker config create gleaner-${PROJECT} ../configs/${PROJECT}/gleanerconfig.yaml`; then
-         echo 'Created gleaner config gleaner-${PROJECT} ${GLEANERIO_GLEANER_CONFIG_PATH}'
-      else
-         echo "ERROR: *** Failed to create config. "
-          exit 1
-      fi
-fi
-
-if [  "$(docker config ls  | grep -${GLEANERIO_NABU_CONFIG_PATH})" ] ; then
-   echo ${GLEANERIO_NABU_CONFIG_PATH} config exists;
-else
-   echo creating config
-
-      if `docker config create nabu-${PROJECT} ../configs/${PROJECT}/nabuconfig.yaml`; then
-         echo 'Created gleaner config  nabu-${PROJECT} ${GLEANERIO_NABU_CONFIG_PATH}'
-      else
-         echo "ERROR: *** Failed to create config. "
-          exit 1
-      fi
-fi
-
-if [  "$(docker config ls  | grep -${GLEANERIO_WORKSPACE_CONFIG_PATH})" ] ; then
-   echo ${GLEANERIO_WORKSPACE_CONFIG_PATH} config exists;
-else
-   echo creating config
-
-      if `docker config create workspace-${PROJECT} ../configs/${PROJECT}/workspace.yaml`; then
-         echo 'Created gleaner config  workspace-${PROJECT} ${GLEANERIO_WORKSPACE_CONFIG_PATH}'
-      else
-         echo "ERROR: *** Failed to create config. "
-          exit 1
-      fi
-fi
