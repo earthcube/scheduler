e535482694a18f24b0747c4b524fda172a1c8595
David Valentine <david.valentine@gmail.com>
2023-09-27 13:25:33 -0700

    update ecrr repo name

--- dagster/implnets/workflows/ecrr/ecrr/repositories/repository.py
+++ dagster/implnets/workflows/ecrr/ecrr/repositories/repository.py
@@ -6,7 +6,7 @@ from jobs.implnet_jobs_ecrr_examples import implnet_job_ecrr_examples
 from sch.implnet_sch_ecrr_examples  import implnet_sch_ecrr_examples
 
 @repository
-def gleaner():
+def ecrr():
 	jobs = [ implnet_job_ecrr_submitted, implnet_job_ecrr_examples]
 	schedules = [ implnet_sch_ecrr_submitted, implnet_sch_ecrr_examples]
 
