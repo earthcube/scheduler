b47e3f5d99b7ffd16724ca76bdc040e3a7060df3
David Valentine <david.valentine@gmail.com>
2023-10-19 14:39:12 -0700

    reworking

--- dagster/implnets/workflows/ingest/ingest/__init__.py
+++ dagster/implnets/workflows/ingest/ingest/__init__.py
@@ -1,80 +1,92 @@
-from dagster import Definitions, load_assets_from_modules
+from dagster import Definitions, load_assets_from_modules, EnvVar
 from dagster_aws.s3.resources import S3Resource
 from dagster_aws.s3.ops import S3Coordinate
 
-from resources.triplestore import BlazegraphResource
+from resources.graph import BlazegraphResource
 from resources.gleanerio import GleanerioResource
-from resources.docker import DockerResource
 
+from pydantic import Field
 from . import assets
 
 all_assets = load_assets_from_modules([assets])
 
-defs = Definitions(
-    assets=all_assets,
-)
 
-DAGSTER_GLEANER_CONFIG_PATH: str = Field(
-    description="DAGSTER_GLEANER_CONFIG_PATH for Project.")
-GLEANER_HEADLESS_NETWORK: str = Field(
-    description="GLEANER_HEADLESS_NETWORK.")
-GLEANERIO_GLEANER_CONFIG_PATH: str = Field(
-    description="GLEANERIO_GLEANER_CONFIG_PATH.")
-GLEANERIO_GLEANER_IMAGE: str = Field(
-    description="GLEANERIO_GLEANER_IMAGE.")
-GLEANERIO_NABU_IMAGE: str = Field(
-    description="GLEANERIO_NABU_IMAGE.")
-GLEANERIO_LOG_PREFIX: str = Field(
-    description="GLEANERIO_LOG_PREFIX.")
-GLEANERIO_GLEANER_DOCKER_CONFIG: str = Field(
-    description="GLEANERIO_GLEANER_DOCKER_CONFIG.")
-GLEANERIO_NABU_DOCKER_CONFIG: str = Field(
-    description="GLEANERIO_NABU_DOCKER_CONFIG.")
 resources = {
     "local": {
         "gleanerio": GleanerioResource(
-            DAGSTER_GLEANER_CONFIG_PATH=EnvVar('DAGSTER_GLEANER_CONFIG_PATH'),
-            GLEANER_HEADLESS_NETWORK=EnvVar('GLEANER_HEADLESS_NETWORK'),
-            GLEANERIO_GLEANER_CONFIG_PATH=EnvVar('GLEANERIO_GLEANER_CONFIG_PATH'),
-            GLEANERIO_NABU_DOCKER_CONFIG=EnvVar('GLEANERIO_NABU_DOCKER_CONFIG'),
-            GLEANERIO_GLEANER_DOCKER_CONFIG=EnvVar('GLEANERIO_GLEANER_DOCKER_CONFIG'),
-            GLEANERIO_NABU_CONFIG_PATH=EnvVar('GLEANERIO_NABU_CONFIG_PATH'),
+            DEBUG=EnvVar('DEBUG'),
+
+            GLEANERIO_DOCKER_URL=EnvVar('GLEANERIO_DOCKER_URL'),
+            GLEANERIO_PORTAINER_APIKEY=EnvVar('GLEANERIO_PORTAINER_APIKEY'),
+
+            GLEANERIO_DOCKER_HEADLESS_NETWORK=EnvVar('GLEANERIO_DOCKER_HEADLESS_NETWORK'),
+            GLEANERIO_HEADLESS_ENDPOINT=EnvVar('GLEANERIO_HEADLESS_ENDPOINT'),
+
             GLEANERIO_GLEANER_IMAGE=EnvVar('GLEANERIO_GLEANER_IMAGE'),
             GLEANERIO_NABU_IMAGE=EnvVar('GLEANERIO_NABU_IMAGE'),
+
+            GLEANERIO_DAGSTER_CONFIG_PATH=EnvVar('GLEANERIO_DAGSTER_CONFIG_PATH'),
+
+
+            GLEANERIO_DOCKER_NABU_CONFIG=EnvVar('GLEANERIO_DOCKER_NABU_CONFIG'),
+            GLEANERIO_DOCKER_GLEANER_CONFIG=EnvVar('GLEANERIO_DOCKER_GLEANER_CONFIG'),
+
+            GLEANERIO_NABU_CONFIG_PATH=EnvVar('GLEANERIO_NABU_CONFIG_PATH'),
+            GLEANERIO_GLEANER_CONFIG_PATH=EnvVar('GLEANERIO_GLEANER_CONFIG_PATH'),
+
             GLEANERIO_LOG_PREFIX=EnvVar('GLEANERIO_LOG_PREFIX'),
+
+            GLEANERIO_DOCKER_CONTAINER_WAIT_TIMEOUT=EnvVar('GLEANERIO_DOCKER_CONTAINER_WAIT_TIMEOUT'),
+
 
         ),
         "minio": S3Resource(),
         "triplestore": BlazegraphResource(
-            GLEANER_GRAPH_URL=EnvVar('GLEANER_GRAPH_URL'),
-            GLEANER_GRAPH_NAMESPACE=EnvVar('GLEANER_GRAPH_NAMESPACE'),
+            GLEANERIO_GRAPH_URL=EnvVar('GLEANERIO_GRAPH_URL'),
+            GLEANERIO_GRAPH_NAMESPACE=EnvVar('GLEANERIO_GRAPH_NAMESPACE'),
         ),
         "triplestore_summary": BlazegraphResource(
-            GLEANER_GRAPH_URL=EnvVar('GLEANER_GRAPH_URL'),
-            GLEANER_GRAPH_NAMESPACE=EnvVar('GLEANER_GRAPH_NAMESPACE'),
+            GLEANERIO_GRAPH_URL=EnvVar('GLEANERIO_GRAPH_URL'),
+            GLEANERIO_GRAPH_NAMESPACE=EnvVar('GLEANERIO_GRAPH_SUMMARY_NAMESPACE'),
         ),
-        "docker": DockerResource(
-            DOCKER_URL=EnvVar('DOCKER_URL'),
-            DOCKER_PORTAINER_APIKEY=EnvVar('DOCKER_PORTAINER_APIKEY'),
-            DOCKER_CONTAINER_WAIT_TIMEOUT=EnvVar('DOCKER_CONTAINER_WAIT_TIMEOUT'),
-        )
+
     },
     "production": {
         "gleanerio": GleanerioResource(
-            account="abc1234.us-east-1",
-            user=EnvVar("DEV_SNOWFLAKE_USER"),
-            password=EnvVar("DEV_SNOWFLAKE_PASSWORD"),
-            database="LOCAL",
-            schema=EnvVar("DEV_SNOWFLAKE_SCHEMA"),
+            DEBUG=EnvVar('DEBUG'),
+
+            GLEANERIO_DOCKER_URL=EnvVar('GLEANERIO_DOCKER_URL'),
+            GLEANERIO_PORTAINER_APIKEY=EnvVar('GLEANERIO_PORTAINER_APIKEY'),
+
+            GLEANERIO_DOCKER_HEADLESS_NETWORK=EnvVar('GLEANERIO_DOCKER_HEADLESS_NETWORK'),
+            GLEANERIO_HEADLESS_ENDPOINT=EnvVar('GLEANERIO_HEADLESS_ENDPOINT'),
+
+            GLEANERIO_GLEANER_IMAGE=EnvVar('GLEANERIO_GLEANER_IMAGE'),
+            GLEANERIO_NABU_IMAGE=EnvVar('GLEANERIO_NABU_IMAGE'),
+
+            GLEANERIO_DAGSTER_CONFIG_PATH=EnvVar('GLEANERIO_DAGSTER_CONFIG_PATH'),
+
+
+            GLEANERIO_DOCKER_NABU_CONFIG=EnvVar('GLEANERIO_DOCKER_NABU_CONFIG'),
+            GLEANERIO_DOCKER_GLEANER_CONFIG=EnvVar('GLEANERIO_DOCKER_GLEANER_CONFIG'),
+
+            GLEANERIO_NABU_CONFIG_PATH=EnvVar('GLEANERIO_NABU_CONFIG_PATH'),
+            GLEANERIO_GLEANER_CONFIG_PATH=EnvVar('GLEANERIO_GLEANER_CONFIG_PATH'),
+
+            GLEANERIO_LOG_PREFIX=EnvVar('GLEANERIO_LOG_PREFIX'),
+
+            GLEANERIO_DOCKER_CONTAINER_WAIT_TIMEOUT=EnvVar('GLEANERIO_DOCKER_CONTAINER_WAIT_TIMEOUT'),
+
+
         ),
         "minio": S3Resource(),
         "triplestore": BlazegraphResource(),
-        "docker": DockerResource()
+
     },
 }
 
-deployment_name = os.getenv("DAGSTER_DEPLOYMENT", "local")
+deployment_name = EnvVar("DAGSTER_DEPLOYMENT", "local")
 
 defs = Definitions(
-    assets=[items, comments, stories], resources=resources[deployment_name]
+    assets=all_assets, resources=resources[deployment_name]
 )
