8acff304a8a85f248375c81fad5b4011d988b0bb
David Valentine <david.valentine@gmail.com>
2023-11-03 17:45:00 -0700

    Stats from missing_graph

--- dagster/implnets/workflows/tasks/tasks/assets/__init__.py
+++ dagster/implnets/workflows/tasks/tasks/assets/__init__.py
@@ -1,2 +1,2 @@
-from .source_stats import loadstats, source_list
+from .source_stats import loadstats, source_list, loadstatsHistory
 from .all_graph_stats import sos_types
--- dagster/implnets/workflows/tasks/tasks/assets/source_stats.py
+++ dagster/implnets/workflows/tasks/tasks/assets/source_stats.py
@@ -32,7 +32,7 @@ def _pythonMinioUrl(url):
 
 def getName(name):
     return name.replace("orgs/","").replace(".nq","")
-@asset()
+@asset(group_name="load")
 def source_list() -> None:
     s3Minio = s3.MinioDatastore(_pythonMinioUrl(GLEANER_MINIO_ADDRESS), MINIO_OPTIONS)
     orglist = s3Minio.listPath(GLEANER_MINIO_BUCKET, ORG_PATH,recursive=False)
@@ -44,8 +44,11 @@ def source_list() -> None:
     with open("data/source_list.json", "w") as f:
         json.dump(list(sources), f)
 #@asset(deps=[source_list])
-@asset(deps=[source_list])
+
+# set a prefix so we can have some named stats file
+@asset(deps=[source_list], group_name="load")
 def loadstats() -> None:
+    prefix = "latest"
     logger = get_dagster_logger()
     s3Minio = s3.MinioDatastore(_pythonMinioUrl(GLEANER_MINIO_ADDRESS),MINIO_OPTIONS)
  #   sourcelist = list(s3Minio.listPath(GLEANER_MINIO_BUCKET, ORG_PATH,recursive=False))
@@ -59,12 +62,51 @@ def loadstats() -> None:
            repo = source.get("name")
            path = f"{REPORT_PATH}{repo}/latest/{STAT_FILE_NAME}"
            s3ObjectInfo = {"bucket_name": GLEANER_MINIO_BUCKET, "object_name": path}
-           resp = s3Minio.getFileFromStore(s3ObjectInfo)
-           stat = json.loads(resp)
-           stat = pick(stat, 'source','sitemap','date','sitemap_count','summoned_count','missing_sitemap_summon_count',
-                       'graph_urn_count','missing_summon_graph_count' )
-           stats.append(stat)
+           try:
+               resp = s3Minio.getFileFromStore(s3ObjectInfo)
+               stat = json.loads(resp)
+               stat = pick(stat, 'source','sitemap','date','sitemap_count','summoned_count','missing_sitemap_summon_count',
+                           'graph_urn_count','missing_summon_graph_count' )
+               stats.append(stat)
+           except Exception as ex:
+               logger.info(f"no missing graph report {source.get('name')}  {ex}")
+
         except Exception as ex:
             logger.info(f"Failed to get { source.get('name')}  {ex}")
     df = pd.DataFrame(stats)
-    df.to_csv("data/weekly_stats.csv")
+    df.to_csv(f"data/{prefix}_stats.csv")
+#@asset( group_name="load")
+@asset(deps=[source_list], group_name="load")
+def loadstatsHistory() -> None:
+    prefix="history"
+    logger = get_dagster_logger()
+    s3Minio = s3.MinioDatastore(_pythonMinioUrl(GLEANER_MINIO_ADDRESS),MINIO_OPTIONS)
+ #   sourcelist = list(s3Minio.listPath(GLEANER_MINIO_BUCKET, ORG_PATH,recursive=False))
+
+    with open("data/source_list.json","r" ) as f:
+        sourcelist = json.load(f)
+    stats = []
+    for source in sourcelist:
+        try:
+           # stat = s3Minio.getReportFile(GLEANER_MINIO_BUCKET,source.get("name"), STAT_FILE_NAME )
+           repo = source.get("name")
+           dirs = s3Minio.listPath( GLEANER_MINIO_BUCKET,f"{REPORT_PATH}{repo}/",recursive=False )
+           for d in dirs:
+               latestpath = f"{REPORT_PATH}{repo}/latest/"
+               if (d.object_name.casefold() == latestpath.casefold()) or (d.is_dir == False):
+                   continue
+               path = f"/{d.object_name}{STAT_FILE_NAME}"
+               s3ObjectInfo = {"bucket_name": GLEANER_MINIO_BUCKET, "object_name": path}
+               try:
+                   resp = s3Minio.getFileFromStore(s3ObjectInfo)
+                   stat = json.loads(resp)
+                   stat = pick(stat, 'source', 'sitemap', 'date', 'sitemap_count', 'summoned_count',
+                               'missing_sitemap_summon_count',
+                               'graph_urn_count', 'missing_summon_graph_count')
+                   stats.append(stat)
+               except Exception as ex:
+                   logger.info(f"no missing graph report {source.get('name')}  {ex}")
+        except Exception as ex:
+            logger.info(f"Failed to get { source.get('name')}  {ex}")
+    df = pd.DataFrame(stats)
+    df.to_csv(f"data/{prefix}_stats.csv")
