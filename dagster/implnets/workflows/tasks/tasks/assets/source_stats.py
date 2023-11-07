78deb8ed7679c8ceadc582a0f903146b9d4fd3e1
David Valentine <david.valentine@gmail.com>
2023-11-07 15:12:10 -0800

    update to dagster 1.5.6 weekly task examples fix compose files with correct variables

--- dagster/implnets/workflows/tasks/tasks/assets/source_stats.py
+++ dagster/implnets/workflows/tasks/tasks/assets/source_stats.py
@@ -47,37 +47,7 @@ def source_list() -> None:
 #@asset(deps=[source_list])
 
 # set a prefix so we can have some named stats file
-@asset(deps=[source_list], group_name="load")
-def loadstats() -> None:
-    prefix = "latest"
-    logger = get_dagster_logger()
-    s3Minio = s3.MinioDatastore(_pythonMinioUrl(GLEANER_MINIO_ADDRESS),MINIO_OPTIONS)
- #   sourcelist = list(s3Minio.listPath(GLEANER_MINIO_BUCKET, ORG_PATH,recursive=False))
 
-    with open("data/source_list.json","r" ) as f:
-        sourcelist = json.load(f)
-    stats = []
-    for source in sourcelist:
-        try:
-           # stat = s3Minio.getReportFile(GLEANER_MINIO_BUCKET,source.get("name"), STAT_FILE_NAME )
-           repo = source.get("name")
-           path = f"{REPORT_PATH}{repo}/latest/{STAT_FILE_NAME}"
-           s3ObjectInfo = {"bucket_name": GLEANER_MINIO_BUCKET, "object_name": path}
-           try:
-               resp = s3Minio.getFileFromStore(s3ObjectInfo)
-               stat = json.loads(resp)
-               stat = pick(stat, 'source','sitemap','date','sitemap_count','summoned_count','missing_sitemap_summon_count',
-                           'graph_urn_count','missing_summon_graph_count' )
-               stats.append(stat)
-           except Exception as ex:
-               logger.info(f"no missing graph report {source.get('name')}  {ex}")
-
-        except Exception as ex:
-            logger.info(f"Failed to get { source.get('name')}  {ex}")
-    df = pd.DataFrame(stats)
-    df.to_csv(f"data/{prefix}_stats.csv")
-    s3Minio.putReportFile(GLEANER_MINIO_BUCKET, repo, f"{prefix}_stats.csv", df.to_csv())
-    get_dagster_logger().info(f"{prefix}_stats.csv uploaded ")
 #@asset( group_name="load")
 @asset(deps=[source_list], group_name="load")
 def loadstatsHistory() -> None:
