6e07753259dcf8fab6110050303c5f221df232b7
David Valentine <david.valentine@gmail.com>
2024-05-24 12:32:43 -0700

    #102 tennant loading prototyping. Setup to for loading releases and creating tenants

--- dagster/implnets/workflows/ingest/ingest/assets/__init__.py
+++ dagster/implnets/workflows/ingest/ingest/assets/__init__.py
@@ -2,10 +2,18 @@ from .gleaner_geocdoes_demo import gleanerio_demo
 from .gleaner_summon_assets import (
     gleanerio_run, release_nabu_run, release_summarize,
     load_report_s3,load_report_graph,
-
     bucket_urls, identifier_stats,
-graph_stats_report
+    graph_stats_report
 )
-from .gleaner_sources import gleanerio_orgs, gleanerio_tennants,  tenant_partitions_def,sources_partitions_def
+from .gleaner_sources import (
+    gleanerio_orgs, gleanerio_tennants,
+    gleanerio_sources,
+    tenant_partitions_def
+    ,sources_partitions_def
+)
 
-from .tenant import  TenantOpConfig, TenantConfig, upload_release,upload_summary, create_tenant_containers, create_graph_namespaces
+from .tenant import  (
+    TenantOpConfig, TenantConfig,
+    upload_release,upload_summary,
+    create_tenant_containers, create_graph_namespaces
+)
