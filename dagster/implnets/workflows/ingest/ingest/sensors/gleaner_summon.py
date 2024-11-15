ebefda770068c44daabf988eb2ce401f8a8bd0ee
David Valentine <david.valentine@gmail.com>
2024-11-15 11:59:44 -0800

    One more place to put project so we can find runs later

--- dagster/implnets/workflows/ingest/ingest/sensors/gleaner_summon.py
+++ dagster/implnets/workflows/ingest/ingest/sensors/gleaner_summon.py
@@ -72,7 +72,7 @@ def sources_schedule(context):
         RunRequest(
             partition_key=partition_key,
          #   run_key=f"{context.scheduled_execution_time}_{partition_key}"
-          run_key=f"summon_asset_{partition_key}"
+          run_key=f"{PROJECT}_summon_asset_{partition_key}"
         )
         for partition_key in partition_keys
     ]
